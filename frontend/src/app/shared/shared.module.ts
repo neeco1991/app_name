5e7484626c81753b4603a82b2064b72cc54b4d1d
Nicola Scionti <nicolascionti@gmail.com>
2022-03-01 08:00:48 +0100

    exporting page overlay, using it in stock summary

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { LoadingComponent } from './components/loading/loading.component';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
+import { PageOverlayComponent } from './components/page-overlay/page-overlay.component';
 
 @NgModule({
-  declarations: [CapitalizePipe, LoadingComponent],
+  declarations: [CapitalizePipe, LoadingComponent, PageOverlayComponent],
   imports: [CommonModule],
-  exports: [CommonModule, CapitalizePipe, LoadingComponent],
+  exports: [
+    CommonModule,
+    CapitalizePipe,
+    LoadingComponent,
+    PageOverlayComponent,
+  ],
 })
 export class SharedModule {}
